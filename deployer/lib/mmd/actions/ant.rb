e41dc19fefeed63670d237beed9c224e7c5061c2
Michael Guymon <michael.guymon@gmail.com>
2010-07-05 23:59:38 -0400

    set base dir for ant

--- deployer/lib/mmd/actions/ant.rb
+++ deployer/lib/mmd/actions/ant.rb
@@ -16,8 +16,9 @@ module MMD
       end
 
       def action
-        ant_call = AntCall.new( File.join( @checkout_path, build_xml ) )
+        ant_call = AntCall.new( @checkout_path, File.join( @checkout_path, build_xml ) )
         @tasks.each do |task|
+          @logger.info( "Running ant task: #{task}")
           ant_call.run_task( task )
         end
       end
