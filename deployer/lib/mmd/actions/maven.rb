92df96becf9083fbad19d0eaf87aa5cc63cfd918
Michael Guymon <michael.guymon@gmail.com>
2010-07-21 18:33:18 -0400

    crude maven support

---
+++ deployer/lib/mmd/actions/maven.rb
@@ -0,0 +1,23 @@
+
+module MMD
+  module Actions
+    module Maven
+
+      def before_action()
+        @goals = []
+        @checkout_path = @parameters[:checkout_path]
+      end
+
+      def action
+        @goals.each do |goal|
+          `cd #{@checkout_path} && mvn -Dmaven.test.skip=true #{goal}`
+        end
+      end
+
+      def goal( task, opts = {} )
+
+        @goals << goal
+      end
+    end
+  end
+end
