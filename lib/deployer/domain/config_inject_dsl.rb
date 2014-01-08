7d3746a45c2876497dc50d8b866decbbbedd1c77
Michael Guymon <michael.guymon@gmail.com>
2014-01-08 00:19:09 -0500

    run server.sh before virtualbox.sh

--- lib/deployer/domain/config_inject_dsl.rb
+++ lib/deployer/domain/config_inject_dsl.rb
@@ -1,3 +1,5 @@
+require 'deployer/etcd/client'
+
 module Deployer
   class Domain::ConfigInjectDsl
 
@@ -7,24 +9,12 @@ module Deployer
       @deploy = deploy
       @config = {}
 
-      ssl_opts = {
-          client_cert: ENV['MMD_CLIENT_CERT'],
-          client_key: ENV['MMD_CLIENT_KEY'],
-          ca_file: ENV['MMD_CA_CERT']
-      }
-
-      @connection = Faraday.new(:url => "https://#{deploy.config.etc_addr}", ssl: ssl_opts) do |faraday|
-        faraday.adapter Faraday::Adapter::Typhoeus
-      end
+      @client = Deployer::Etcd::Client.connect
     end
 
     def set( name )
-      response = @connection.get "/app/#{@plan.namespace}/config/#{name}"
-      result = JSON.parse(response.body)
-      if result['node'] && result['node']['value']
-        @config[name.to_s] = result['node']['value']
-      end
-
+      @config[name.to_s] = @client.get "/app/#{@deploy.plan.namespace}/config/#{name}"
+      
       self
     end
 
